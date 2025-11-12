beb50fbd64ef8fe5fc0044fe8ff170d8ad10d350
Aung Min Myat <aungminmyat2212@gmail.com>
2025-11-12 14:54:47 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-// 👇 Tell React Router that your app lives inside /Portfolio.github.io/
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename="/Portfolio.github.io">
     <App />
